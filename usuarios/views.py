3fad6775bf2d6d56382ff2463443069618cd9b73
Carlos Garcia <garcas93@gmail.com>
2025-12-08 19:54:59 -0600

    statics, templates, app cliente urls views

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
-# Create your views here.
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect('cliente:cliente-list')
+
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user:
+            login(request, user)
+            return redirect('cliente:cliente-list')
+        else:
+            return render(request, "usuarios/login.html", {
+                "error": "Usuario o contraseña incorrectos."
+            })
+
+    return render(request, "usuarios/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse('usuarios:login'))
