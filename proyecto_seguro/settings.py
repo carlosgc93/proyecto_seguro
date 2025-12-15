1df4130252b0f6633b240fa214ed12e7f23ae663
Carlos Garcia <garcas93@gmail.com>
2025-12-15 10:28:14 -0600

    implementacion django-environ, variables de entorno

--- proyecto_seguro/settings.py
+++ proyecto_seguro/settings.py
@@ -9,6 +9,8 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
+
+import environ
 import os
 from pathlib import Path
 
@@ -16,14 +18,18 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
+env = environ.Env()
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+wnidqutgk&sf%+edtib()rpo3#m)dtpql1^6b=!t_tz=nukg^'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
